90044b5b5417c7f08674978e489c65dc77c89786
João <429267+antao@users.noreply.github.com>
2022-12-18 21:07:19 +0100

    Add bubble sort

---
+++ Bubble/Program.cs
@@ -0,0 +1,21 @@
+﻿// https://en.wikipedia.org/wiki/Bubble_sort
+Console.WriteLine("Bubble Sort");
+
+var array = new[] {3, 9, 1, 42, 10};
+var flag = true;
+
+for (int j = 0; j <= array.Length - 2 && flag; j++)
+{
+    flag = false;
+    for (int i = 0; i <= array.Length - 2; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            (array[i + 1], array[i]) = (array[i], array[i + 1]);
+            flag = true;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(",", array));
+Console.ReadKey();
