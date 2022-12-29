812f6fdecddc4646a4d804d4839f2c1e85fe65cb
João <429267+antao@users.noreply.github.com>
2022-12-29 13:02:52 +0100

    Add insertion sort

---
+++ Insertion/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Insertion Sort");
+
+int[] array = {110, 1, 21, 53, 8, 98, 26, 163, 38, 897};
+
+int i;
+for (i = 1; i < array.Length; i++) 
+{ 
+    var key = array[i]; 
+    var j = i - 1;
+    while (j >= 0 && array[j] > key) 
+    { 
+        array[j + 1] = array[j]; 
+        j -= 1; 
+    } 
+    array[j + 1] = key; 
+} 
+
+Console.WriteLine(string.Join(",", array));
+Console.ReadKey();
